86e7c384b76d510184190a1f033200d44c97b7a5
Adrian Mohabir <mohabiradrian@gmail.com>
2019-12-19 23:52:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,11 +7,12 @@ class School
   end
   
   def add_student(name, grade)
-   if @roster.has_key?(grade)
-     @roster[grade] << name 
-   else 
-     @roster[grade] = [name]
-   end 
+  # if @roster.has_key?(grade)
+  #   @roster[grade] << name 
+  # else 
+  #   @roster[grade] = [name]
+  # end 
+  self.roster[grade] ||= []
    end 
    
    def grade(grade)
